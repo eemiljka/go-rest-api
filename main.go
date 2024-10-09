e155e4ff19dda68b2bb690caf580e2df83ec0798
eemiljka <eemiljka@metropolia.fi>
2024-10-09 22:57:35 +0300

    made some changes, made POST again

--- main.go
+++ main.go
@@ -1,49 +1,80 @@
 package main
 
+// Imports
 import (
 	"fmt"
 	"log"
 	"net/http"
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 )
 
+// Article struct
 type Article struct {
-	Title string `json:"Title"`
-	Desc string `json:"desc"`
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
 	Content string `json:"content"`
 }
 
-type Articles []Article
+// Articles array (fake database)
+var Articles []Article
 
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
-	}
+/*********** Functions*************/
 
-	fmt.Println("Endpoint Hit: All Articles Endpoint")
-	json.NewEncoder(w).Encode(articles)
+// Return all articles
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: returnAllArticles")
+    json.NewEncoder(w).Encode(Articles)
 }
 
-func testPostArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Test POST endpoint worked")
+// Return single article
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    key := vars["id"]
+
+    for _, article := range Articles {
+        if article.Id == key {
+            json.NewEncoder(w).Encode(article)
+        }
+    }
 }
 
+// Create new article
+func createNewArticle(w http.ResponseWriter, r *http.Request) { 
+    reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	Articles = append(Articles, article)
+    json.NewEncoder(w).Encode(article)
+}
+
+// Home page function
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint Hit")
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
-func handleRequests() {
 
+// Handle requests (routing)
+func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 
-	myRouter.HandleFunc("/", homePage)
-	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
-	myRouter.HandleFunc("/articles", testPostArticles).Methods("POST")
-
-	log.Fatal(http.ListenAndServe(":8081", myRouter))
+    myRouter.HandleFunc("/", homePage)
+    myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+    log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
+
+// Main function
 func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	Articles = []Article{
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
 	handleRequests()
 }
